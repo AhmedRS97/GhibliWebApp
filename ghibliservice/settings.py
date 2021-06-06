fd55bce248a051b8fc326107d96f584620182248
Ahmed Ramadan <ahmedr@protonmail.com>
2021-06-06 17:52:53 +0200

    Added environment variables and basic settings.

--- ghibliservice/settings.py
+++ ghibliservice/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!2lpct%o@p-$y$3$@rb3-k3_q6fg9%l(=ykoih9&)s1tsnkpld'
+SECRET_KEY = env("SECRET_KEY", default='django-insecure-!2lpct%o@p-$y$3$@rb3-k3_q6fg9%l(=ykoih9&)s1tsnkpld')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
